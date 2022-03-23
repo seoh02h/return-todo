ee0a04123c4375d589f14d4c6531242fb0eb4fe5
seoh02h <seok02h@khu.ac.kr>
2022-03-23 11:42:52 +0900

    Spring Boot 프로젝트 생성

---
+++ return-todo-server/src/main/java/com/seohyuni/returntodoserver/ReturnTodoServerApplication.java
@@ -0,0 +1,13 @@
+package com.seohyuni.returntodoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReturnTodoServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ReturnTodoServerApplication.class, args);
+  }
+
+}
